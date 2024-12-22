9a425dc3056f133839118274ef1a7aed80bbae0c
Duran Brooks <brooks@Durans-MacBook-Air.local>
2024-12-22 10:32:25 -0500

    Update anime retrieval method name

--- src/main/java/com/kumakun/favouriteslist/controller/AnimeController.java
+++ src/main/java/com/kumakun/favouriteslist/controller/AnimeController.java
@@ -2,6 +2,7 @@ package com.kumakun.favouriteslist.controller;
 
 import com.kumakun.favouriteslist.dto.AnimeDTO;
 import com.kumakun.favouriteslist.service.AnimeService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,8 @@ import reactor.core.publisher.Mono;
 
 import java.util.List;
 
+///TODO Introduce Spring Security to authorize request origin
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("api/v1/favourites/anime")
 public class AnimeController {
